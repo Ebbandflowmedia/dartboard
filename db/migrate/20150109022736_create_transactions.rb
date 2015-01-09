068ed741e644b770eb1ccf22a6a03f399661aacb
EFMedia <nate@ebbandflowmedia.com>
2015-01-09 08:52:20 -0700

    Generate Transactions

---
+++ db/migrate/20150109022736_create_transactions.rb
@@ -0,0 +1,16 @@
+class CreateTransactions < ActiveRecord::Migration
+  def self.up
+    create_table :transactions do |t|
+      t.string :name
+      t.belongs_to :account, index:true
+      t.integer :amount
+      t.timestamps
+    end
+
+  end
+  def self.down
+    drop_table :transactions
+    	
+   
+  end
+end
